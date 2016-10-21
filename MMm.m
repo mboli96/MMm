2a3d3f4124f0bba813f58060c30b39a583c3e5b9
mboli96 <mboli96@gmail.com>
2016-10-21 11:36:18 +0200

    Comienzo

---
+++ MMm.m
@@ -0,0 +1,16 @@
+% Vamos a hacer una función que dados el tiempo de servicio, el tiempo de
+% llegada de usuarios y el número de servidores calcule el tiempo promedio
+% que tarda un usuario de salir del sistema
+
+function [TiempoMedio] = MMm(lambda, mu, m)
+    ro = (lambda / (m*mu));
+    sumatoria = 0;
+    for n = 0:m-1
+        sumatoria = sumatoria + ((lambda / mu)^n) / factorial(n);
+    end
+    Po = 1 / (sumatoria + (((lambda / mu)^m) / factorial(m)) * (m * mu / (m*mu - lambda)))
+    Pq = (Po * (m^m) * ro^m) / (factorial(m) * (1 - ro));
+    
+    Tiempo_medio = (1 / mu) + (ro * Pq) / (lambda * (1-ro))
+end
+        
